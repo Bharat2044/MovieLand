4316c69db08c4c8eff9e1caf9adfd0a902134ee4
Bharat2044 <bk9955204456@gmail.com>
2024-07-15 19:15:30 +0530

    code added

---
+++ src/MovieCard.jsx
@@ -0,0 +1,23 @@
+const MovieCard = ({ movie: { imdbID, Year, Poster, Title, Type } }) => {
+  return (
+    <div className="movie" key={imdbID}>
+      <div>
+        <p>{Year}</p>
+      </div>
+
+      <div>
+        <img
+          src={Poster !== "N/A" ? Poster : "https://img.freepik.com/free-vector/hand-painted-watercolor-galaxy-background_23-2148973123.jpg"}
+          alt={Title}
+        />
+      </div>
+
+      <div>
+        <span>{Type}</span>
+        <h3>{Title}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
